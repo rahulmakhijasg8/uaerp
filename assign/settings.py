712e8cf7c2549f992c37077d916a1aea902c7753
Your Name <rahulmakhijals7@gmail.com>
2021-12-27 21:08:04 +0530

    white

--- assign/settings.py
+++ assign/settings.py
@@ -123,16 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR/ 'static']
+STATIC_ROOT = BASE_DIR/ 'staticfiles'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
-MEDIA_URL = '/images/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'assign\static\images')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/ 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- assign/urls.py
+++ assign/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('',include('django.contrib.auth.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
